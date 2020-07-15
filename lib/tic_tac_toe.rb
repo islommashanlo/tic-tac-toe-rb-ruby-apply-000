405176905d1b491b1ac682b4d54cea3a41363e39
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-15 17:26:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,10 @@
 #board
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board(pos)
-  puts " #{pos[0]} | #{pos[1]} | #{pos[2]} "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{pos[3]} | #{pos[4]} | #{pos[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{pos[6]} | #{pos[7]} | #{pos[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
