75c52563080cd07d6c102fe96eaffdc3c35fbcf9
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-15 17:36:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,10 +48,22 @@ def turn(board)
   end
 end
 #turn_count
+#turn_count
 def turn_count(board)
-  counter = 0
-  until counter == 9
-    turn(board)
-    counter += 1
+  turns = 0
+  board.each do |pos|
+    if pos == "X" or pos == "O"
+      then turns += 1
+    end
+  end
+  return turns
+end
+#current_player
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    then return "X"
+  else
+    return "O"
   end
 end
